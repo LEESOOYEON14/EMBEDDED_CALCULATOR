778b199389ef37fb9b4bf6b025b02f6a46aad0b1
dsy9090@naver.com <dsy9090@naver.com>
2018-12-01 01:39:30 -0800

    add blur ( vertical )

--- source/blur.c
+++ source/blur.c
@@ -52,6 +52,57 @@ void horizontal_blur(Bitmap *bitmap, int radius) {
     bitmap->pixel_array = blurred_array;
 }
 
+void vertical_blur(Bitmap *bitmap, int radius) {
+    int width = bitmap->info_header->width;
+    int height = bitmap->info_header->height;
+
+    byte *blurred_array = malloc(width * height * 3);
+
+    for (int col = 0; col < width; col++) {
+        int sum_b = 0;
+        int sum_g = 0;
+        int sum_r = 0;
+        int count = 0;
+
+        for (int i = -radius; i <= radius; i++) {
+            Pixel *pixel = get_pixel(bitmap, i, col);
+            sum_b += pixel->b;
+            sum_g += pixel->g;
+            sum_r += pixel->r;
+            count++;
+        }
+
+        byte avg_b = (byte) (sum_b / count);
+        byte avg_g = (byte) (sum_g / count);
+        byte avg_r = (byte) (sum_r / count);
+
+        set_pixel(blurred_array, width, 0, col, avg_b, avg_g, avg_r);
+
+        for (int row = 1; row < height; row++) {
+            Pixel *pixel = get_pixel(bitmap, row-radius-1, col);
+            sum_b -= pixel->b;
+            sum_g -= pixel->g;
+            sum_r -= pixel->r;
+
+            pixel = get_pixel(bitmap, row+radius, col);
+            sum_b += pixel->b;
+            sum_g += pixel->g;
+            sum_r += pixel->r;
+
+            avg_b = (byte) (sum_b / count);
+            avg_g = (byte) (sum_g / count);
+            avg_r = (byte) (sum_r / count);
+
+            set_pixel(blurred_array, width, row, col, avg_b, avg_g, avg_r);
+        }
+    }
+
+    free(bitmap->pixel_array);
+
+    bitmap->pixel_array = blurred_array;
+}
+
+
 void set_pixel(byte *pixel_array, int width, int row, int col, byte b, byte g, byte r) {
     pixel_array[row*(width*3) + (col*3+B)] = b;
     pixel_array[row*(width*3) + (col*3+G)] = g;
