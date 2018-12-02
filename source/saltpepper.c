e835651bda7d42be6b3052dc13a208ee596ef1d4
sangik <sangik125@naver.com>
2018-12-02 22:30:20 +0900

    edited saltpepper

--- source/saltpepper.c
+++ source/saltpepper.c
@@ -1,19 +1,23 @@
-#include <iostream>
+
+#include <stdlib.h>
 #include "../header/saltpepper.h"
 #include "../header/bitmap.h"
+
+
+
 
 void saltpepper(Bitmap *bitmap, int radius)
 {
   int width = bitmap->info_header->width;
-  int height = bitmap->info_header->height
+  int height = bitmap->info_header->height;
   
-  int num_noise_pixel = (int){(double)(width*height*3)*radius};
+  int noise_pixel = (int){(double)(width*height*3)*radius};
   
   byte *saltpepper_array = malloc(width * height * 3);
 
   for(int row = 0; row < height; row++)
   {
-    for(int i =0; i <num_noise_pixel; i++ )
+    for(int i =0; i <noise_pixel; i++ )
     {
       int r = rand() % width;
       int c = rand() % height;
@@ -21,7 +25,7 @@ void saltpepper(Bitmap *bitmap, int radius)
       int pixel_g =0;
       int pixel_r =0;
 
-      int ch = rand % 3;
+      int ch = rand() % 3;
       Pixel *pixel = get_pixel(bitmap,r,c);
 
       pixel_b = pixel->b;
@@ -30,6 +34,7 @@ void saltpepper(Bitmap *bitmap, int radius)
 
       noise_pixel = (rand() % 2 ==1 ) ? 255: 0;
     
-      set_pixel(saltpepper_array, width,row,0,noise_pixel, noise_pixel, noixe_pixel);
-  }
+      set_pixel(saltpepper_array, width,row,0,noise_pixel, noise_pixel, noise_pixel);
+    }
+  }  
 }
