1194f68b9c745a9812985c5b1bfc4e22ec86c290
dsy9090@naver.com <dsy9090@naver.com>
2018-12-05 01:53:30 -0800

    add main header

---
+++ header/main.h
@@ -0,0 +1,18 @@
+#define NUM_OPTIONS 8
+#define PARAM_LENGTH 100
+
+#define OPT_HELP 0
+#define OPT_OUT 1
+#define OPT_THRESH 2
+#define OPT_INVERT 3
+#define OPT_MONO 4
+#define OPT_CROP 5
+#define OPT_BLUR 6
+
+#define CROP_DIMENSIONS 4
+
+#define FALSE 0
+#define TRUE !FALSE
+
+void print_usage(void);
+void print_dimensions(Bitmap *bitmap);
