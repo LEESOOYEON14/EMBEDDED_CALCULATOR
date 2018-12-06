1d24b0677c4ff03111335691a884d91f4563c035
dsy9090@naver.com <dsy9090@naver.com>
2018-12-05 22:26:08 -0800

    make main.c

---
+++ main/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <errno.h>
+
+#include "bitmap.h"
+#include "bitmap_write.h"
+
+#include "threshold.h"
+#include "invert.h"
+#include "crop.h"
+#include "monochrome.h"
+#include "blur.h"
+#include "saltpepper.h"
+
+#include "main.h"
+
+
